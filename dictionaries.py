b2c1b2f7cab267f9142080f120bcd76b873e5b23
rash039 <107994195+rash039@users.noreply.github.com>
2025-05-20 15:24:19 +0530

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -3,4 +3,11 @@ a = {'a': 1, 'b': 2}
 a.get('c', 'NA')
 # Output:
 # NA
-  
+
+
+
+# for loop
+a = [1,23,345,34564,76,78, 34,523]
+>>> for index, fruit in enumerate(a):
+...    print(f"{a[index]}")
+
